51bfff9747fc2f6983017e5f0e1af55698d5e096
pandathegiant <jie.lu@ge.com>
2018-05-01 03:25:18 -0400

    Update index.js

--- src/components/view/Navigation/index.js
+++ src/components/view/Navigation/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
-import AppBar from 'material-ui/AppBar';
-import IconMenu from 'material-ui/IconMenu';
+import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import IconButton from 'material-ui/IconButton';
@@ -12,19 +11,16 @@ import { blueGrey600, cyan600 } from 'material-ui/styles/colors'
 import ExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
 import AssignmentInd from 'material-ui/svg-icons/action/assignment-ind';
 import ChromeReaderModeIcon from 'material-ui/svg-icons/action/chrome-reader-mode';
-import CreditCardIcon from 'material-ui/svg-icons/action/credit-card';
-import RedeemIcon from 'material-ui/svg-icons/action/redeem';
-// import TimelineIcon from 'material-ui/svg-icons/action/timeline';
-import LocalPlayIcon from 'material-ui/svg-icons/maps/local-play';
-// import LocalOfferIcon from 'material-ui/svg-icons/maps/local-offer';
-// import MoodIcon from 'material-ui/svg-icons/social/mood';
-import PollIcon from 'material-ui/svg-icons/social/poll';
-// import PeopleIcon from 'material-ui/svg-icons/social/people';
+import PeopleIcon from 'material-ui/svg-icons/social/people';
 import PersonIcon from 'material-ui/svg-icons/social/person';
-// import WhatsHotIcon from 'material-ui/svg-icons/social/whatshot';
-// import StarIcon from 'material-ui/svg-icons/toggle/star';
-// import AddCircleIcon from 'material-ui/svg-icons/content/add-circle';
-import ModeEditIcon from 'material-ui/svg-icons/editor/mode-edit';
+import PersonOutlineIcon from 'material-ui/svg-icons/social/person-outline';
+import GroupIcon from 'material-ui/svg-icons/social/group';
+import GroupAddIcon from 'material-ui/svg-icons/social/group-add';
+import PersonAddIcon from 'material-ui/svg-icons/social/person-add';
+import EventNoteIcon from 'material-ui/svg-icons/notification/event-note';
+import ExitToAppIcon from 'material-ui/svg-icons/action/exit-to-app';
+
+import Avatar from 'material-ui/Avatar';
 
 import { getImageForEnv } from 'static/images/index'
 
@@ -36,20 +32,10 @@ class Navigation extends React.Component {
         super(props);
         this.navigateToRoute = this.navigateToRoute.bind(this);
         this.themePalette = this.props.muiTheme.palette;
+        this.state = {open: false};
     }
-    
-    buildIconMenu (actions) {
-        const { logout } = actions;
-        return (
-            <IconMenu
-                iconButtonElement={<IconButton><ExpandMoreIcon /></IconButton>}
-                anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
-                targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-            >
-                <MenuItem onTouchTap={logout} primaryText="Logout" />
-            </IconMenu>
-        );
-    };
+
+    handleToggle = () => this.setState({open: !this.state.open});
 
     navigateToRoute (someArg, evt, tabEl) {
         // close any open event item
@@ -58,20 +44,37 @@ class Navigation extends React.Component {
         this.props.history.push(tabEl.props['data-route']);
     };
 
+    getMenuItemHandler (routeName)
+    {
+        var that = this;
+        return function ()
+        { 
+            that.setState({open: false});
+            that.props.history.push(routeName);
+        };    
+    }
+
+    handleLogout = () => {this.setState({open: false}); this.props.logout()}
+
     render () {
         const { user, logout } = this.props;
-        
-        const iconMenu = this.buildIconMenu({ logout });
+
+        // TODO: restore this back in once google authentication issue is resolved for me.
+        // if the user hasn't logged in, then render nothing.
+        // if (!user.uid || 0 === user.uid.length) return (<div/>);
 
         return (
             <div>
-                
-                {<AppBar
-                    title={'Welcome, ' + user.displayName}
-                    iconElementRight={iconMenu}
-                >
-                </AppBar>}
-
+                <IconButton onClick={this.handleToggle} style={{width: 60, height: 60, padding: 0}}>
+                    <Avatar size={60} icon={<PersonOutlineIcon/>}/>
+                </IconButton>
+                {'Welcome, ' + user.displayName}
+                <Drawer docked={false} width={200} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+                    <MenuItem onClick={this.getMenuItemHandler("contact")} primaryText="Contact" leftIcon={<PersonIcon/>}/>
+                    <MenuItem onClick={this.getMenuItemHandler("reports")} primaryText="Report" leftIcon={<EventNoteIcon/>}/>
+                    <MenuItem onClick={this.getMenuItemHandler("users")} primaryText="Add Users" leftIcon={<GroupAddIcon/>}/>
+                    <MenuItem onClick={this.handleLogout} primaryText="Logout" leftIcon={<ExitToAppIcon/>}/>
+                </Drawer>
             </div>
         );  
     }
