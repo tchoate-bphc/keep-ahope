938e8eb122c2a332547fd137b61730c549e9ac16
Camden Hill <camden.hill@ge.com>
2018-05-16 09:45:21 -0400

    intake page

--- src/components/view/AuthorizedRoute/index.js
+++ src/components/view/AuthorizedRoute/index.js
@@ -8,7 +8,7 @@ class AuthorizedRoute extends Component {
 	constructor(props) {
 		super(props);
     }
-    
+
     render() {
 
         const { location, userPermissions, component: Component, ...rest } = this.props;
@@ -16,18 +16,11 @@ class AuthorizedRoute extends Component {
         const notAuthProps = {
             from: location
         };
-        
-        const pageWrapperStyles = {
-            position: 'fixed',
-            width: '100%',
-            height: 'calc(100% - 175px)',
-            // height: userPermissions.admin ? 'calc(100% - 247px)' : 'calc(100% - 175px)',
-        }
 
         return (
             <Route {...rest} render={props => (
                 userPermissions.basic ? (
-                        <div className='page-wrapper' style={pageWrapperStyles}>
+                        <div className='page-wrapper'>
                             <Component {...props}/>
                         </div>
                     ) : (
