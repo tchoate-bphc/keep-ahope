76bf0e63b1c6428db756340796f6c85e811a165b
Camden Hill <camden.hill@ge.com>
2018-05-20 20:12:04 -0400

    added a bulk of functionality to the intake form

--- src/components/view/Intake/IntakeForm.js
+++ src/components/view/Intake/IntakeForm.js
@@ -18,6 +18,7 @@ import { List, ListItem } from 'material-ui/List';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import { RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import DatePicker from 'material-ui/DatePicker';
 
 import { createEvent, getContact } from 'actions';
 
@@ -30,32 +31,111 @@ class IntakeForm extends Component {
 
   constructor(props) {
     super(props);
+
     this.submitForm = this.submitForm.bind(this)
+    this.toggleRefill = this.toggleRefill.bind(this)
+    this.checkNarcanOffered = this.checkNarcanOffered.bind(this)
+    this.checkNarcanGiven = this.checkNarcanGiven.bind(this)
+    this.selectReferrals = this.selectReferrals.bind(this)
+    this.selectRace = this.selectRace.bind(this)
+    this.setEventDate = this.setEventDate.bind(this)
+    this.setDateOfBirth = this.setDateOfBirth.bind(this)
+    this.updateCheckNewContact = this.updateCheckNewContact.bind(this)
+    this.updateCheckOutreach = this.updateCheckOutreach.bind(this)
+
     this.themePalette = this.props.muiTheme.palette
+
+    const todayDate = new Date();
+    todayDate.setFullYear(todayDate.getFullYear());
+    todayDate.setHours(0, 0, 0, 0);
+
+    const initDateOfBirth = new Date(1980, 0, 1)
+
     this.state = {
-      value: 1,
       newContact: false,
-      intake: {
-        eventData: {
-          daily: {
-            countHelpingOthers: 2,
-            countSyringesGiven: 10,
-            countSyringesTaken: 10,
-            enrollmentOrRefill: "Enrollment",
-            narcanGiven: false,
-            narcanOffered: true
-          },
-          firstContact: false,
-        },
-        contactData: {
-          uid: "jnde123199abc",
-        }
+      outreach: false,
+      toggleRefillLabel: "No Refill",
+      narcanOfferedLabel: "No",
+      narcanGivenLabel: "No",
+      eventData: {
+        eventDate: todayDate,
+        refill: "no",
+        syringesGiven: "N/A",
+        syringesTaken: "N/A",
+        referrals: "No Referrals",
+        narcanOffered: "no",
+        narcanGiven: "no",
+        numPeople: "N/A",
+        eventNotes: ""
+      },
+      contactData: {
+        uid: "jnde123199abc",
+        dateOfBirth: initDateOfBirth,
+        countryOfBirth: "United States",
+        profileNotes: "",
+        race: "White",
+        gender: "Male",
+        ageOfFirstInjection: 0,
       }
     }
   }
 
-  handleChange(event, index, value) {
-    this.setState({value})
+  toggleRefill() {
+    if (this.state.toggleRefillLabel == "Refill") {
+      this.setState({toggleRefillLabel: "No Refill"})
+    } else {
+      this.setState({toggleRefillLabel: "Refill"})
+    }
+  }
+
+  checkNarcanOffered() {
+    var eventData = {...this.state.eventData}
+    if (this.state.eventData.narcanOffered == "no") {
+      this.setState({narcanOfferedLabel: "Yes"})
+      eventData.narcanOffered = "yes";
+      this.setState({eventData})
+    } else {
+      this.setState({narcanOfferedLabel: "No"})
+      eventData.narcanOffered = "no";
+      this.setState({eventData})
+    }
+  }
+
+  checkNarcanGiven() {
+    var eventData = {...this.state.eventData}
+    if (this.state.eventData.narcanGiven == "no") {
+      this.setState({givenLabel: "Yes"})
+      eventData.narcanGiven = "yes";
+      this.setState({eventData})
+    } else {
+      this.setState({givenLabel: "No"})
+      eventData.narcanGiven = "no";
+      this.setState({eventData})
+    }
+  }
+
+  selectReferrals(event, index, value) {
+    var eventData = {...this.state.eventData}
+    eventData.referrals = value
+    this.setState({eventData})
+  }
+
+  selectRace(event, index, value) {
+    var contactData = {...this.state.contactData}
+    contactData.race = value
+    this.setState({contactData})
+  }
+
+  setEventDate(event, value) {
+    var eventData = {...this.state.eventData}
+    eventData.eventDate = value
+    this.setState({eventData})
+  }
+
+  setDateOfBirth(event, value) {
+    var contactData = {...this.state.contactData}
+    contactData.dateOfBirth = value
+    this.setState({contactData})
   }
 
   submitForm() {
@@ -76,6 +156,9 @@ class IntakeForm extends Component {
     this.setState({newContact: !this.state.newContact})
   }
 
+  updateCheckOutreach() {
+    this.setState({outreach: !this.state.outreach})
+  }
 
   render() {
 
@@ -103,9 +186,16 @@ class IntakeForm extends Component {
     const newContact = this.state.newContact ? (
       <Card className="card">
         <div className="fields">
-          <div>
-            <TextField hintText="Date of Birth" />
-          </div>
+
+          <DatePicker
+            hintText="Date of Birth"
+            floatingLabelText="Date of Birth"
+            value={this.state.contactData.dateOfBirth}
+            onChange={this.setDateOfBirth}
+            autoOk={true}
+            openToYearSelection={true}
+          />
+
           <div>
             <div style={labelStyle}>
               Gender
@@ -143,37 +233,24 @@ class IntakeForm extends Component {
           </div>
 
           <div>
-            <div style={labelStyle}>
-              Race
-            </div>
+            <div style={labelStyle}>Race/Ethnicity</div>
             <SelectField
-              // value={this.state.value}
-              // onChange={this.handleChange}
+              value={this.state.contactData.race}
+              onChange={this.selectRace}
               selectedMenuItemStyle={selectedMenuItemStyles}
               id="race"
             >
-              <MenuItem primaryText="White" />
+              <MenuItem primaryText="American Indian" value="AI" />
+              <MenuItem primaryText="Asian" value="Asian" />
+              <MenuItem primaryText="Black/African American" value="BAA" />
+              <MenuItem primaryText="Hawaiian/Pacific Islander" value="HPI "/>
+              <MenuItem primaryText="White" value="White" />
+              <MenuItem primaryText="Other" value="Other" />
+              <MenuItem primaryText="Prefer not to say" value="Refuse" />
             </SelectField>
           </div>
 
-          <div>
-            <div style={labelStyle}>
-              Ethnicity
-            </div>
-            <SelectField
-              // value={this.state.value}
-              // onChange={this.handleChange}
-              selectedMenuItemStyle={selectedMenuItemStyles}
-              id="ethnicity"
-            >
-              <MenuItem primaryText="Polish" />
-            </SelectField>
-          </div>
-
-          <div style={labelStyle}>
-            Hispanic
-          </div>
-          <Toggle label="No" defaultToggled={false} labelPosition="right" />
+          <Toggle label="Hispanic" defaultToggled={false} labelPosition="right" />
 
           <div>
             <div style={labelStyle}>
@@ -204,71 +281,114 @@ class IntakeForm extends Component {
       <div></div>
     )
 
+    const outreach = this.state.outreach ? (
+      <Card className="card">
+        <div className="fields">
+          <div id="syringesGiven">
+            <div style={labelStyle}>Syringes Given</div>
+            <Slider
+              name="SyringesGiven"
+              defaultValue={0}
+              step={1}
+              min={0}
+              max={10}
+            />
+          </div>
+
+          <div id="syringesTaken">
+            <div style={labelStyle}>Syringes Taken</div>
+            <Slider
+              name="SyringesTaken"
+              defaultValue={0}
+              min={0}
+              max={10}
+            />
+          </div>
+        </div>
+      </Card>
+    ) : (
+      <div></div>
+    )
+
     return (
       <div className="page">
-        <div className="form">
+        <form className="form">
+
+          <Card className="card">
+            <div className="fields">
+              <DatePicker
+                hintText="Date"
+                floatingLabelText="Date"
+                value={this.state.eventData.eventDate}
+                onChange={this.setEventDate}
+                autoOk={true}
+              />
+            </div>
+          </Card>
+
           <Card className="card">
             <CardTitle style={titleColorStyle}>
               Form Questions
             </CardTitle>
             <div className="checkbox">
               <Checkbox
-                label="Intake"
+                label="Standard Questions"
                 defaultChecked={true}
+                disabled={true}
               />
               <Checkbox
-                label="New"
+                label="First Contact"
                 onCheck={this.updateCheckNewContact.bind(this)}
               />
-              <Checkbox label="Periodic" />
+              <Checkbox
+                label="Return Visit"
+                onCheck={this.updateCheckOutreach.bind(this)}
+              />
             </div>
           </Card>
 
           {newContact}
 
+          {outreach}
+
           <Card className="card">
             <div className="fields">
-              <Toggle label="No Refill" defaultToggled={false} labelPosition="right" />
 
-              <div>
-                <TextField hintText="Date" />
-              </div>
-
-              <div id="syringesGiven">
-                <div style={labelStyle}>Syringes Given</div>
-                <Slider name="SyringesGiven" defaultValue={0} step={1} min={0} max={10} />
-              </div>
-
-              <div id="syringesTaken">
-                <div style={labelStyle}>Syringes Taken</div>
-                <Slider name="SyringesTaken" defaultValue={0} min={0} max={10} />
-              </div>
+              <Toggle
+                label={this.state.toggleRefillLabel}
+                defaultToggled={false}
+                labelPosition="right"
+                className="toggle"
+                onClick={(e) => this.toggleRefill(e)}
+              />
 
               <SelectField
-                value={this.state.value}
-                onChange={this.handleChange}
+                value={this.state.eventData.referrals}
+                onChange={this.selectReferrals}
                 selectedMenuItemStyle={selectedMenuItemStyles}
                 id="referrals"
+                floatingLabelText="Any referrals?"
               >
-                <MenuItem value={1} primaryText="No Referrals" />
-                <MenuItem value={2} primaryText="Referrals" />
+                <MenuItem value="No Referrals" primaryText="No Referrals" />
+                <MenuItem value="Referrals" primaryText="Referrals" />
               </SelectField>
 
-              <div className="checkbox usedGiven">
-                <div>
-                  <div style={labelStyle}>Used</div>
+              <div className="checkbox narcanOfferedGiven">
+                <div className="narcanOffered">
+                  <div style={labelStyle}>Narcan Offered</div>
                   <Checkbox
-                    label="No"
-                    id="ifUsed"
-                    defaultChecked={true}
+                    label={this.state.narcanOfferedLabel}
+                    defaultChecked={false}
+                    onClick={() => this.checkNarcanOffered()}
                   />
                 </div>
 
-                <div>
-                  <div style={labelStyle}>Given</div>
+                <div className="narcanGiven">
+                  <div style={labelStyle}>Narcan Given</div>
                   <Checkbox
-                    label="No"
-                    id="ifGiven"
+                    label={this.state.narcanGivenLabel}
+                    defaultChecked={false}
+                    onClick={() => this.checkNarcanGiven()}
                   />
                 </div>
               </div>
@@ -318,7 +438,7 @@ class IntakeForm extends Component {
               <FlatButton label="Save" primary={true} onClick={this.submitForm} />
             </div>
           </Card>
-        </div>
+        </form>
       </div>
     )
 
